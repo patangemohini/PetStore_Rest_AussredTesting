d5654f45d2fad7ef887980653ef291bd64bf5712
mohini patange <patangemohini5@gmail.com>
2022-08-23 11:51:23 +0530

    Added : Tested user module of petstore

---
+++ src/test/java/com/bridgelabz/petstorerestausserdtesting/PetStoreRestAussredTesting.java
@@ -0,0 +1,105 @@
+package com.bridgelabz.petstorerestausserdtesting;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class PetStoreRestAussredTesting {
+
+    @Test(priority = 0)
+    public void acreateUser() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 0,\n" +
+                        "  \"username\": \"Mohini\",\n" +
+                        "  \"firstName\": \"Mohini\",\n" +
+                        "  \"lastName\": \"Patange\",\n" +
+                        "  \"email\": \"Mohinipatange39@gmail.com\",\n" +
+                        "  \"password\": \"Mohi@1234\",\n" +
+                        "  \"phone\": \"9350127300\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}")
+                .when()
+                .post("https://petstore.swagger.io/v2/user");
+        response.prettyPrint();
+        Assertions.assertEquals(200,response.statusCode());
+    }
+
+    @Test(priority = 1)
+    public void buserName() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://petstore.swagger.io/v2/user/Mohini");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test(priority = 5)
+    public void eupdatedUser() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "  \"id\": 0,\n" +
+                        "  \"username\": \"Rohini\",\n" +
+                        "  \"firstName\": \"Rohini\",\n" +
+                        "  \"lastName\": \"Patange\",\n" +
+                        "  \"email\": \"rose23@gmail.com\",\n" +
+                        "  \"password\": \"rose@1234\",\n" +
+                        "  \"phone\": \"9349592321\",\n" +
+                        "  \"userStatus\": 0\n" +
+                        "}")
+                .when()
+                .put("https://petstore.swagger.io/v2/user/Mohini");
+        response.prettyPrint();
+        Assertions.assertEquals(200,response.statusCode());
+    }
+
+    @Test(priority = 6)
+    public void fdeleteUser() {
+        Response response = given()
+                .accept("application/json")
+                .when()
+                .get("https://petstore.swagger.io/v2/user/Rohini");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test(priority = 3)
+    public void ccreateWithList() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body("[\n" +
+                        "  {\n" +
+                        "    \"id\": 10,\n" +
+                        "    \"username\": \"Pooja\",\n" +
+                        "    \"firstName\": \"Pooja\",\n" +
+                        "    \"lastName\": \"Swami\",\n" +
+                        "    \"email\": \"pooja@gmail.com\",\n" +
+                        "    \"password\": \"swami@1234\",\n" +
+                        "    \"phone\": \"9653645300\",\n" +
+                        "    \"userStatus\": 0\n" +
+                        "  }\n" +
+                        "]")
+                .when()
+                .post("https://petstore.swagger.io/v2/user/createWithList");
+        response.prettyPrint();
+        Assertions.assertEquals(200,response.statusCode());
+    }
+
+    @Test(priority = 4)
+    public void dlogin() {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://petstore.swagger.io/v2/user/login?username=Mohini&password=Mohi%401234");
+        response.prettyPrint();
+        response.then().assertThat().statusCode(200);
+    }
+}
