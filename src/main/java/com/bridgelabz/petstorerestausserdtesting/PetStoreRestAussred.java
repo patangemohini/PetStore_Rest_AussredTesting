7a5341f1ca4a39646e0d7a3bb5df35998ba81546
mohini patange <patangemohini5@gmail.com>
2022-08-23 11:40:30 +0530

    Added : first Message

---
+++ src/main/java/com/bridgelabz/petstorerestausserdtesting/PetStoreRestAussred.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.petstorerestausserdtesting;
+
+public class PetStoreRestAussred {
+    public static void main(String[] args) {
+        System.out.println("Pet store rest assured test cases ");
+    }
+}
